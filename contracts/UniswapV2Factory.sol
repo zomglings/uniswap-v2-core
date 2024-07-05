12e203363cf775ac11cdcdbf3ca37527185769b3
Neeraj Kashyap <nkashy1@gmail.com>
2024-07-04 18:25:55 -0700

    Removed all interface inheritance

--- contracts/UniswapV2Factory.sol
+++ contracts/UniswapV2Factory.sol
@@ -3,7 +3,7 @@ pragma solidity >=0.5.16;
 import './interfaces/IUniswapV2Factory.sol';
 import './UniswapV2Pair.sol';
 
-contract UniswapV2Factory is IUniswapV2Factory {
+contract UniswapV2Factory {
     address public feeTo;
     address public feeToSetter;
 
