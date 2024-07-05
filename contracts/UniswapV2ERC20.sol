e20f92bd6ba662abd2f9de01d453132d78bf7acf
Neeraj Kashyap <nkashy1@gmail.com>
2024-07-04 18:24:54 -0700

    Trying to make this work

--- contracts/UniswapV2ERC20.sol
+++ contracts/UniswapV2ERC20.sol
@@ -3,7 +3,7 @@ pragma solidity >=0.5.16;
 import './interfaces/IUniswapV2ERC20.sol';
 import './libraries/SafeMath.sol';
 
-contract UniswapV2ERC20 is IUniswapV2ERC20 {
+contract UniswapV2ERC20 {
     using SafeMath for uint;
 
     string public constant name = 'Uniswap V2';
@@ -18,9 +18,8 @@ contract UniswapV2ERC20 is IUniswapV2ERC20 {
     bytes32 public constant PERMIT_TYPEHASH = 0x6e71edae12b1b97f4d1f60370fef10105fa2faae0126114a169c64845d6126c9;
     mapping(address => uint) public nonces;
 
-    // Note: Removed because they are inherited from the interface.
-    // event Approval(address indexed owner, address indexed spender, uint value);
-    // event Transfer(address indexed from, address indexed to, uint value);
+    event Approval(address indexed owner, address indexed spender, uint value);
+    event Transfer(address indexed from, address indexed to, uint value);
 
     constructor() public {
         // Note: Using block.chainid here instead of the previous yul code.
